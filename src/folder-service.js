fbbf72459ced9b8b853fb747417ac7b1a733e125
aniledev <elinamcgill@gmail.com>
2021-01-28 10:51:47 -0500

    CHORE: create comment block

---
+++ src/folder-service.js
@@ -0,0 +1,8 @@
+// create method to get all folders using knex
+
+// create method to insert a folder using knex
+// create method to get a specific folder
+// create method to delete a folder
+// create method to update a folder
+
+module.exports = foldersService;
