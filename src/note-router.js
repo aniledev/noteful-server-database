35f9b42fd24a9bcd2430558bde36106ee47f0922
aniledev <elinamcgill@gmail.com>
2021-01-28 11:49:30 -0500

    FEAT: add necessary import statements to router

---
+++ src/note-router.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const xss = require('xss');
+const notesService = require('./notes-service');
+
+const noteRouter = express.Router();
+const bodyParser = express.json();
+
+const serializedNote = note => ({
+    note_id : note.note_id,
+    note_name : xss(note.note_name),
+    content : xss(note.content),
+    folder_id : note.folder_id,
+    modified : note.modified,
+})
