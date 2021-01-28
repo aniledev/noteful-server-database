410ff4f8b65ad0956a956ebd04d2bfa9b0a5c0c5
aniledev <elinamcgill@gmail.com>
2021-01-28 16:43:43 -0500

    CHORE: update seed file with correct syntax

--- seeds/seed.noteful.sql
+++ seeds/seed.noteful.sql
@@ -1,26 +1,18 @@
+
 insert into folders
     (folderName)
 values
-    ("colors"),
-    ("fruits"),
-    ("clothes");
+    ('fruits'),
+    ('clothes'),
+    ('colorspurple');
 
 insert into notes
     (noteName, content, folderId)
 values
-    ("purple", "purple", 1),
-    ("orange", "orange", 1),
-    ("shirt", "shirt", 3),
-    ("pants", "pants", 3),
-    ("banana", "banana", 2),
-    ("blue", "blue", 1),
-    ("shoes", "shoes", 3),
-    ("apple", "apple", 2),
-    ("grapes", "grapes", 2),
-    ("yellow", "yellow", 1),
-    ("jeans", "jeans", 3),
-    ("white", "white", 1),
-    ("peaches", "peaches", 2),
-    ("jacket", "jacket", 3),
-    ("mango", "mango", 2),
-    ("belt", "belt", 3);
+    ('apple' , 'apple', 1 ),
+    ('shirt', 'shirt' , 2),
+    ('grapes' , 'grapes' , 1),
+    ('purple', 'purple' , 3),
+    ('jacket' , 'jacket' , 2),
+    ('peaches' , 'peaches' , 1),
+    ('orange' , 'orange' , 3);
