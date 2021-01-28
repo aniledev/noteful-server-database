0a2248f308329cabd8b8c7cd125a4612a69357cc
aniledev <elinamcgill@gmail.com>
2021-01-28 11:40:29 -0500

    FEAT: flesh out getting all notes using knex sql queries

--- src/note-service.js
+++ src/note-service.js
@@ -1,6 +1,13 @@
+const { format } = require("morgan");
+
 // create a service to get all notes using knex
+const notesService = {
+    getAllNotes(knex){ return knex.select("*")from("notes")}
+}
 // insert a single note using knex
 // get a single note by its id
 // delete a note by its id
+
+
 
 module.exports = notesService;
