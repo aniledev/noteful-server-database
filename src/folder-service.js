8c8293519b746a46fd239de345a6e4d224022e22
aniledev <elinamcgill@gmail.com>
2021-01-28 10:52:54 -0500

    FEAT: create service to get all folders using knex

--- src/folder-service.js
+++ src/folder-service.js
@@ -1,5 +1,9 @@
 // create method to get all folders using knex
-
+const foldersService = {
+  getAllFolders(knex) {
+    return knex.select("*").from("folders");
+  },
+};
 // create method to insert a folder using knex
 // create method to get a specific folder
 // create method to delete a folder
