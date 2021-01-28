8023dab36a0c2ada9a5834e1e3484831f5446f50
aniledev <elinamcgill@gmail.com>
2021-01-28 11:58:31 -0500

    FEAT: add POST / routes

--- src/note-router.js
+++ src/note-router.js
@@ -17,7 +17,7 @@ const serializedNote = (note) => ({
 // create get and post route for /
 noteRouter
   .route("/")
-  .get((res, res, next) => {
+  .get((req, res, next) => {
     const db = req.app.get("db");
     notesService
       .getAllNotes(db)
@@ -26,12 +26,33 @@ noteRouter
       })
       .catch(next);
   })
-  .post();
+  .post(bodyParser, (req, res, next) => {
+    // use object destructing to access the req body and the newNote object
+    const { note_name, content, folder_id } = req.body;
+    const newNote = { note_name, content, folder_id };
+
+    // validate to ensure that each field of the request body is present/entered
+    for (const [key, value] of Object.entries(newNote))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+
+    notesService
+      .insertNotes(req.app.get("db"), newNote)
+      .then((note) => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${note.note_id}`))
+          .json(serializedNote(note));
+      })
+      .catch(next);
+  });
 
 // create get, delete and patch route for /:note_id
 noteRouter
   .route("/:note_id")
-  .get((res, res, next) => {})
+  .get((req, res, next) => {})
   .delete()
   .patch();
 
