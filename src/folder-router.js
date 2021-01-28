9898ad458400f6ba72fde48e808809d7a5fcf349
aniledev <elinamcgill@gmail.com>
2021-01-28 10:43:24 -0500

    FEAT: flesh out GET / route

--- src/folder-router.js
+++ src/folder-router.js
@@ -1,21 +1,27 @@
-const path = require('path');
-const express = require('express');
-const xss = require('xss');
-const foldersService  = require('./folders-service');
-
+const path = require("path");
+const express = require("express");
+const xss = require("xss");
+const foldersService = require("./folders-service");
 
 const folderRouter = express.Router();
 const jsonParser = express.json();
 
-const serializedFolder = folder => ({
-    folder_id : folder.folder_id,
-    folder_name : xss(folder.folder_name)
+const serializedFolder = (folder) => ({
+  folder_id: folder.folder_id,
+  folder_name: xss(folder.folder_name),
 });
 
 // create get and post routes
-folderRouter.route("/")
+folderRouter.route("/").get((req, res, next) => {
+  const db = req.app.get("db");
+  foldersService
+    .getAllFolders(db)
+    .then((folders) => {
+      res.json(folders.map(serializedFolder));
+    })
+    .catch(next);
+})
+
 
 // create get, delete, and patch routes
-folderRouter.route(":folder_id")
-
-module.exports = folderRouter;
+folderRouter.route(":folder_id").module.exports = folderRouter;
