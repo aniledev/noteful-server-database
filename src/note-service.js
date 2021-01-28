4762b5de81254fd30031309db7ff351ba6c57e50
aniledev <elinamcgill@gmail.com>
2021-01-28 11:42:02 -0500

    FEAT: add method to insert a singlar note

--- src/note-service.js
+++ src/note-service.js
@@ -2,12 +2,21 @@ const { format } = require("morgan");
 
 // create a service to get all notes using knex
 const notesService = {
-    getAllNotes(knex){ return knex.select("*")from("notes")}
-}
+  getAllNotes(knex) {
+    return knex.select("*").from("notes");
+  },
+  insertNotes(knex, newNote) {
+    return knex
+      .insert(newNote)
+      .into("notes")
+      .returning("*")
+      .then((rows) => {
+        return rows[0];
+      });
+  },
+};
 // insert a single note using knex
 // get a single note by its id
 // delete a note by its id
-
-
 
 module.exports = notesService;
