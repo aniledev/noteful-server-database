81531fcb2153a0803f6bd7ad24c1e9b78c16f233
aniledev <elinamcgill@gmail.com>
2021-01-28 14:09:59 -0500

    FEAT: update server.js file to use knex and config variables

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 const app = require("./app");
-const { PORT, NODE_ENV } = require("./config");
+const { PORT, DATABASE_URL } = require("./config");
+const knex = require("knex");
+
+const pg = require("pg");
+
+pg.defaults.ssl = process.env.NODE_ENV === "production";
+
+const db = knex({
+  client: "pg",
+  connection: DATABASE_URL,
+});
+
+app.set("db", db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
