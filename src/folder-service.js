0f2b48ded769be15b0e60dbbf513eaa0777fadaf
aniledev <elinamcgill@gmail.com>
2021-01-28 10:54:17 -0500

    FEAT: create service to insert a singular folder

--- src/folder-service.js
+++ src/folder-service.js
@@ -3,6 +3,15 @@ const foldersService = {
   getAllFolders(knex) {
     return knex.select("*").from("folders");
   },
+  insertFolder(knex, newFolder) {
+    return knex
+      .insert(newFolder)
+      .into("folders")
+      .returning("*")
+      .then((rows) => {
+        return rows[0];
+      });
+  },
 };
 // create method to insert a folder using knex
 // create method to get a specific folder
