cc891350fbec354f9f183f2af9a8a2ccefa82b59
aniledev <elinamcgill@gmail.com>
2021-01-29 10:32:31 -0500

    CHORE: update package.json for deployment

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    "postgres://pougaszzdvcqtm:2abdf6c5610749dc3147f97b9cef30c08b3266fa82bdf8cadab8599b4274f156@ec2-52-72-162-207.compute-1.amazonaws.com:5432/ddiav22bg4ovrn",
+    "postgres://elinamcgill@localhost/noteful-server-database",
 };
